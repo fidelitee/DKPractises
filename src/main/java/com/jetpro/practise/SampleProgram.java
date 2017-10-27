308c04fb66506ce31fdd649b2234af6347b41a79
manee2k6 <manee2k6@gmail.com>
2017-10-27 09:29:44 +0530

    JetPro project artifacts are added here.

---
+++ src/main/java/com/jetpro/practise/SampleProgram.java
@@ -0,0 +1,12 @@
+package com.jetpro.practise;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to DevOps Training..");
+
+	}
+
+}

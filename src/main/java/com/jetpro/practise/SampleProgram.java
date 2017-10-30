4d61763e6fb0ff37208c2e450b6775a289fabd77
manee2k6 <manee2k6@gmail.com>
2017-10-30 09:01:01 +0530

    modified sample program for Maven Jetpro project

--- src/main/java/com/jetpro/practise/SampleProgram.java
+++ src/main/java/com/jetpro/practise/SampleProgram.java
@@ -6,6 +6,7 @@ public class SampleProgram {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Welcome to DevOps Training..");
+		System.out.println("Welcome to Maven JetPro project");
 
 	}
 
